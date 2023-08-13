421ea83d17c20e1ce69cb9e0b9c4d5f54c5a8926
Vadym <46047631+VadymKolesov@users.noreply.github.com>
2023-08-13 16:35:09 +0200

    Added "up btn"

--- src/js/up-btn.js
+++ src/js/up-btn.js
@@ -1,9 +1,9 @@
 const scrollBtn = document.querySelector('.isShowBtn');
 
 window.onscroll = () => {
-  if (window.scrollY > 1700) {
+  if (window.scrollY > 1000) {
     scrollBtn.classList.remove('isHiddenBtn');
-  } else if (window.scrollY < 1700) {
+  } else if (window.scrollY < 1000) {
     scrollBtn.classList.add('isHiddenBtn');
   }
 };
